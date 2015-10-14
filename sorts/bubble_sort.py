9e3b01b4b07140cc341e8981cec36a0792439acb
Hill Wyrough <alexander.hill.wyrough@gmail.com>
2015-10-14 17:56:10 -0400

    Adds bubble sort

---
+++ sorts/bubble_sort.py
@@ -0,0 +1,19 @@
+from create_list import random_list, is_sorted
+
+
+def bubble_sort(my_list):
+    while not is_sorted(my_list):
+        for i in range(len(my_list) - 1):
+            if my_list[i] > my_list[i + 1]:
+                tmp = my_list[i]
+                my_list[i] = my_list[i + 1]
+                my_list[i + 1] = tmp
+    return my_list
+
+my_list = random_list(50)
+print(my_list)
+print(is_sorted(my_list))
+
+sorted_list = bubble_sort(my_list)
+print(sorted_list)
+print(is_sorted(sorted_list))
